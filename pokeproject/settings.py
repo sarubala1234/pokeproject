2d64dd35fe769c5caab97dd027e6cb107ff4467f
Saru Bala <jdsarubala1234@gmail.com>
2025-08-08 02:55:33 +0530

    Add SECRET_KEY fallback error

--- pokeproject/settings.py
+++ pokeproject/settings.py
@@ -23,7 +23,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable not set")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
